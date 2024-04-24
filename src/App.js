73f31abcb13dd2ae1457ef679935a5e90c8a3355
Adiam Ahmed <adu.adu911@gmail.com>
2024-04-24 16:35:39 -0400

    add card components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import '../src/styles/partials/global.scss';
 import Hero from "./components/Hero/Hero";
+import FeatureCard from "./components/FeatureCard/FeatureCard";
 
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
+      <FeatureCard />
+      
 
     </div>
   );
---
+++ src/components/FeatureCard/FeatureCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../UI/Card/Card'
+import './FeatureCard.scss'
+
+const FeatureCard = () => {
+    return (
+        <div className='feature-container'>
+            <h1>Features </h1>
+            <Card  />
+
+        </div>
+    );
+};
+
+export default FeatureCard;
