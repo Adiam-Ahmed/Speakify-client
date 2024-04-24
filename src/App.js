398cb1dce5bdafad3edfa11ab420d0bec8afdd63
Adiam Ahmed <adu.adu911@gmail.com>
2024-04-24 14:38:35 -0400

    Add Hero component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import '../src/styles/partials/global.scss';
+import Hero from "./components/Hero/Hero";
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
 
     </div>
   );
