90e503b29bac8a20e7825d4526d70b8365a95045
Adiam Ahmed <adu.adu911@gmail.com>
2024-04-25 02:31:25 -0400

    add login page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import Header from "./components/Header/Header";
-import '../src/styles/partials/global.scss';
-import Hero from "./components/Hero/Hero";
-import FeatureCard from "./components/FeatureCard/FeatureCard";
-import Footer from "./components/Footer/Footer";
+import './App.scss';
+// import Header from "./components/Header/Header";
+// import Hero from "./components/Hero/Hero";
+// import FeatureCard from "./components/FeatureCard/FeatureCard";
+// import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
+
 
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header />
       <Hero />
       <FeatureCard />
-      <Footer />
+      <Footer /> */}
+      <Login />
       
 
     </div>
---
+++ src/components/Login/Login.js
@@ -0,0 +1,62 @@
+import './Login.scss'
+import { useState } from 'react'
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import CTAButton from '../UI/CTAButton/CTAButton';
+
+
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const handleUsernameChange = e => setUsername(e.target.value)
+    const handlePasswordChange = e => setPassword(e.target.value)
+
+    const handleLogin = async e => {
+        e.preventDefault()
+
+    }
+
+    return (
+        <section className='login'>
+            <h1 className='login__header'> User Login</h1>
+            <form>
+                <div className='login__container'>
+                    <AccountCircleRoundedIcon sx={{ fontSize: 35 }} />
+                    <label htmlFor="user_name" className='login__box'>
+                        <input
+                            className='login__input'
+                            type="text"
+                            id="user_name"
+                            name='user_name'
+                            placeholder='Username'
+                            value={username}
+                            onChange={handleUsernameChange}
+                        />
+                    </label>
+                </div>
+                <div className='login__container'>
+                    <LockOpenIcon sx={{ fontSize: 35 }} />
+                    <label htmlFor="password" className='login__box'>
+                        <input
+                            className='login__input'
+                            type="password"
+                            id="password"
+                            name='password'
+                            placeholder='Password'
+                            value={password}
+                            onChange={handlePasswordChange}
+                        />
+                    </label>
+                </div>
+                <div className='login__container'>
+                    <CTAButton className="button-add" text="Login" btnType="hero" onClick={handleLogin} />
+                </div>
+            </form>
+            <p>No account? Sign Up here</p>
+        </section>
+    )
+}
+
+export default Login
