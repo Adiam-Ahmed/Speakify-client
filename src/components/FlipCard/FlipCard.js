c08802747c735d0cd9504926df32d269c6e78962
Adiam Ahmed <adu.adu911@gmail.com>
2024-05-01 07:08:52 -0400

    fix style in flip card

--- src/components/FlipCard/FlipCard.js
+++ src/components/FlipCard/FlipCard.js
@@ -39,11 +39,11 @@ const FlipCard = () => {
                             <img src={placeholder} alt="placeholder" className="placeholder" />
                             <div>
                                 <h3>The answer of 2+2 =4</h3>
-                                <p>Description</p>
                             </div>
-                            
                         </div>
-                    
+                        <div>
+                            <p>Description:Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        </div>
                         <div className="toggle-btn">
                             <ToggleButton
                                 value="check"
