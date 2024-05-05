2c070ac7d2ed7d7275e305ed4ac90cf377ce8770
Adiam Ahmed <adu.adu911@gmail.com>
2024-05-05 13:20:43 -0400

    add header style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,24 +2,33 @@ import React from 'react';
 import './Header.scss'
 import Logo from '../../assests/Logo/speakify-favicon-white.png'
 import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import { Link } from 'react-router-dom'
 
 
 const Header = () => {
     return (
         <header className='header'>
-            <div className='header__container'>
-                <img
-                    src={Logo}
-                    alt='logo'
-                    className='header__icon'
-                />
-                <h3 className='header__logo'>SPEAKIFY</h3>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <img
+                        src={Logo}
+                        alt='logo'
+                        className='header__icon'
+                    />
+                    <h3 className="text-lg font-bold" href='logo'>SPEAKIFY</h3>
+                </div>
+                <div className="flex justify-end flex-1 px-2">
+                    <div className="flex items-stretch">
+                        <div className="dropdown dropdown-end">
+                            <div tabIndex={0} role="button" className="btn btn-ghost rounded-btn"><PersonOutlinedIcon sx={{ fontSize: 50 }} /></div>
+                            <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
+                                <li><Link to='/login'>Login</Link></li>
+                                <li><Link to='/signup'>Sign Up </Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='header__profile-icon'>
-                <PersonOutlinedIcon sx={{ fontSize: 60 }} />
-            </div>
-
-            
 
         </header>
     );
