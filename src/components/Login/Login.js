43f521d72a673e45193891095e415cf0aa5ee188
Adiam Ahmed <adu.adu911@gmail.com>
2024-06-09 14:50:45 -0400

    change style for login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -78,61 +78,72 @@ const Login = ({ handleLoginHeader }) => {
 
 
     return (
-        <section className="login">
-            <h1 className="login__header">User Login</h1>
-            <form onSubmit={handleLogin}>
-                <div className="login__container">
-                    <AccountCircleRoundedIcon sx={{ fontSize: 35 }} />
-                    <label htmlFor="login_username" className="login__box">
-                        <input
-                            className="login__input"
-                            type="text"
-                            id="login_username"
-                            name="user_name"
-                            placeholder="Username"
-                            value={username}
-                            onChange={handleUsernameChange}
-                        />
-                        {fieldErrors.username && <p className="error-message">{fieldErrors.username}</p>}
-                    </label>
-                    
+        <>
+            <section className="login">
+                <h1 className="login__header">User Login</h1>
+                <div className="flex flex-col w-full lg:flex-row  justify-center ">
+                    <section className='col-one'>
+                        <form onSubmit={handleLogin}>
+                            <div className="login__container">
+                                <AccountCircleRoundedIcon sx={{ fontSize: 35 }} />
+                                <label htmlFor="login_username" className="login__box">
+                                    <input
+                                        className="login__input"
+                                        type="text"
+                                        id="login_username"
+                                        name="user_name"
+                                        placeholder="Username"
+                                        value={username}
+                                        onChange={handleUsernameChange}
+                                    />
+                                    {fieldErrors.username && <p className="error-message">{fieldErrors.username}</p>}
+                                </label>
+
+                            </div>
+                            <div className="login__container">
+                                <LockOpenIcon sx={{ fontSize: 35 }} />
+                                <label htmlFor="login_password" className="login__box">
+                                    <input
+                                        className="login__input"
+                                        type="password"
+                                        id="login_password"
+                                        name="password"
+                                        placeholder="Password"
+                                        value={password}
+                                        onChange={handlePasswordChange}
+                                    />
+                                    {fieldErrors.password && <p className="error-message">{fieldErrors.password}</p>}
+                                </label>
+                            </div>
+                            <div className="login__container">
+                                <CTAButton
+                                    className="button-add"
+                                    text={'Login'}
+                                    btnType="hero"
+                                />
+                            </div>
+                        </form>
+                    </section>
+                    <div className="divider divider-primary lg:divider-horizontal my-4 lg:mx-4"></div>
+                    <section className='col-two flex flex-col justify-evenly gap-15'>
+                        <p className='login__paragraph'>No account? <br />  Sign Up <span className='login__bold-primary text-primary'><Link to='/signup'>here</Link></span> or login using Google Account</p>
+                        <div className="sign-in-google">
+                            <GoogleLogin
+                                onSuccess={handleGoogleSignUp}
+                                onError={() => {
+                                    console.log('Login Failed')
+                                }}
+                                cookiePolicy={'single_host_origin'}
+                            />
+                        </div>
+
+                    </section>
                 </div>
-                <div className="login__container">
-                    <LockOpenIcon sx={{ fontSize: 35 }} />
-                    <label htmlFor="login_password" className="login__box">
-                        <input
-                            className="login__input"
-                            type="password"
-                            id="login_password"
-                            name="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={handlePasswordChange}
-                        />
-                        {fieldErrors.password && <p className="error-message">{fieldErrors.password}</p>}
-                    </label>
-                </div>
-                <div className="login__container">
-                    <CTAButton
-                        className="button-add"
-                        text={'Login'}
-                        btnType="hero"
-                    />
-                </div>
-            </form>
-            <p className='login__paragraph'>No account? Sign Up <span className='login__bold-primary text-primary'><Link to='/signup'>here</Link></span> or login using Google Account</p>
-            <div className="sign-in-google">
-                <GoogleLogin
-                    onSuccess={handleGoogleSignUp}
-                    onError={() => {
-                        console.log('Login Failed')
-                    }}
-                    cookiePolicy={'single_host_origin'}
-                />
-            </div>
+            </section>
+        </>
 
 
-        </section>
+
     )
 }
 
